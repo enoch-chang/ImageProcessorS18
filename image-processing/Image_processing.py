0609acc8974824a5a2e3e570ecaf9dcf2be8a715
an-schneider <35575344+an-schneider@users.noreply.github.com>
2018-04-20 13:10:48 -0400

    add very basic histogram equalization capability
    Functionality verified using test images from http://marvinproject.sourceforge.net/en/plugins/histogramEqualization.html

--- image-processing/Image_processing.py
+++ image-processing/Image_processing.py
@@ -1,1 +1,20 @@
+import os
+import skimage
+import numpy
+import matplotlib
+import numpy as np
+import png
 
+from skimage import io
+from skimage import data, img_as_float
+from skimage import exposure
+
+# Load Image
+image_name = 'bad_contrast.JPEG'
+image = io.imread(image_name)
+
+
+# Equalization
+img_eq = exposure.equalize_hist(image)
+
+skimage.io.imsave('hist_equalized.JPEG', img_eq, plugin=None)
