32fbcd3c0f1a792cae8fe0076b05f11b8ea3b859
an-schneider <35575344+an-schneider@users.noreply.github.com>
2018-04-28 17:07:13 -0400

    convert test code to object oriented paradigm

--- image-processing/test.py
+++ image-processing/test.py
@@ -1,22 +1,26 @@
-import Image_processing
+from Image_processing import Image
 from skimage import io
 import pytest
 
 
 def test_reverse_video():
-    image = io.imread('color_image_test.JPEG')
-    file_type = '.JPEG'
-    output = Image_processing.reverse_video(image, 'color', file_type)
-    inverted_1 = [255, 255, 255] - image[50, 50]
-    inverted_2 = [255, 255, 255] - image[0, 0]
+    test = Image(file_name='color_image_test', file_ext='.JPEG',
+                 color_type='color')
+    test.gather_data()
+    output = test.reverse_video()
+    inverted_1 = [255, 255, 255] - test.image_array[50, 50]
+    inverted_2 = [255, 255, 255] - test.image_array[0, 0]
     assert output[50, 50].all() == inverted_1.all()
     assert output[0, 0].all() == inverted_2.all()
 
 
 def test_gather_data():
-    image_info, image_array, image_name = Image_processing.gather_data(
-        'color_image_test', '.JPEG')
-    assert image_info['size'] == [1200, 777]
-    assert image_info['color_type'] == 'color'
+    test = Image(file_name='color_image_test', file_ext='.JPEG',
+                 color_type='color')
+    test.gather_data()
+    assert test.dimensions == [1200, 777]
+    assert test.color_type == 'color'
+
 
 test_reverse_video()
+test_gather_data()
