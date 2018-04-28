6a1b6dcab800c32d34ef2ac79572e44dd42f509a
an-schneider <35575344+an-schneider@users.noreply.github.com>
2018-04-28 17:06:40 -0400

    convert all main code to object oriented paradigm

--- image-processing/Image_processing.py
+++ image-processing/Image_processing.py
@@ -15,72 +15,88 @@ from skimage import exposure
 from skimage import util
 
 
-# Load and Gather Image Data (Size, color / greyscale, file type)
-def gather_data(filename, file_type):
-    image_name = filename + file_type
-    image_array = io.imread(image_name)
-    image_info = {}
-    rows, columns, channels = image_array.shape
-    dimensions = [columns, rows]
-    if channels == 1:
-        color_type = 'greyscale'
-    else:
-        color_type = 'color'
-    image_info = {'size': dimensions,
-                  'color_type': color_type}
-    print(image_info)
-    return image_info, image_array, image_name
+class Image:
+
+    def __init__(self, file_name=None, file_ext=None,
+                 color_type=None,
+                 image_array=None, dimensions=None,
+                 contrast_stretch_array=None, hist_eq_array=None,
+                 rev_video_array=None):
+        self.file_name = file_name
+        self.file_ext = file_ext
+        self.color_type = color_type
+        self.image_array = image_array
+        self.contrast_stretch_array = contrast_stretch_array
+        self.hist_eq_array = hist_eq_array
+        self.rev_video_array = rev_video_array
+        self.dimensions = dimensions
+
+    # Load and Gather Image Data (Size, color / greyscale, file type)
+    def gather_data(self):
+        self.image_array = io.imread(self.file_name+self.file_ext)
+        image_info = {}
+        rows, columns, channels = self.image_array.shape
+        self.dimensions = [columns, rows]
+        if channels == 1:
+            self.color_type = 'greyscale'
+        else:
+            self.color_type = 'color'
+        return self.color_type, self.dimensions, self.image_array
+
+    # Generate Histogram
+    def show_histogram(self):
+        if self.color_type == 'color':
+            color = ('b', 'g', 'r')
+            for i, col in enumerate(color):
+                hist = cv2.calcHist([self.image_array], [i], None, [256], [0,
+                                                                           256
+                                                                           ])
+                plt.plot(hist, color=col)
+                plt.xlim([0, 256])
+                plt.xlabel('Intensity'), plt.ylabel('Frequency (# of Pixels)')
+                plt.title('Histogram for %s' % self.file_name)
+            plt.show()
+        elif self.color_type == 'greyscale':
+            plt.hist(self.image_array.ravel(), 256, [0, 256], color='black')
+            plt.xlabel('Intensity'), plt.ylabel('Frequency (# of Pixels)')
+            plt.title('Histogram for %s' % self.filename)
+            plt.show()
+
+    # Equalization
+    def hist_eq(self):
+        self.hist_eq_array = exposure.equalize_hist(self.image_array)
+        skimage.io.imsave('hist_equalized'+self.file_ext, self.hist_eq_array,
+                          plugin=None)
+        return self.hist_eq_array
+
+    # Contrast Stretching
+    def contrast_stretch(self):
+        p2 = np.percentile(self.image_array, 2)
+        p98 = np.percentile(self.image_array, 98)
+        self.contrast_stretch_array = exposure.rescale_intensity(
+            self.image_array, in_range=(p2, p98))
+        skimage.io.imsave('contrast_stretched'+self.file_ext,
+                          self.contrast_stretch_array, plugin=None)
+        return self.contrast_stretch_array
+
+    # Reverse Video
+    def reverse_video(self):
+        inverted = np.zeros_like(self.image_array)
+        if self.color_type == 'greyscale':
+            inverted = util.invert(self.image_array)
+        elif self.color_type == 'color':
+            rows, columns, channels = self.image_array.shape
+            for row in range(0, rows):
+                for column in range(0, columns):
+                    inverted[row, column] = [255, 255, 255] - \
+                                            self.image_array[row, column]
+        skimage.io.imsave('reverse_video' + self.file_ext, inverted,
+                          plugin=None)
+        self.rev_video_array = inverted
+        return self.rev_video_array
 
 
-# Generate Histogram
-def show_histogram(img, color_type, image_name):
-    if color_type == 'color':
-        color = ('b', 'g', 'r')
-        for i, col in enumerate(color):
-            hist = cv2.calcHist([img], [i], None, [256], [0, 256])
-            plt.plot(hist, color=col)
-            plt.xlim([0, 256])
-        plt.xlabel('Intensity'), plt.ylabel('Frequency (# of Pixels)')
-        plt.title('Histogram for %s' % image_name)
-        plt.show()
-    elif color_type == 'greyscale':
-        plt.hist(img.ravel(), 256, [0, 256], color='black')
-        plt.xlabel('Intensity'), plt.ylabel('Frequency (# of Pixels)')
-        plt.title('Histogram for %s' % image_name)
-        plt.show()
-
-
-# Equalization
-def hist_eq(img, file_type):
-    img_eq = exposure.equalize_hist(img)
-    skimage.io.imsave('hist_equalized'+file_type, img_eq, plugin=None)
-    return img_eq
-
-
-# Contrast Stretching
-def contrast_stretch(img, file_type):
-    p2 = np.percentile(img, 2)
-    p98 = np.percentile(img, 98)
-    img_contrast_stretched = exposure.rescale_intensity(img, in_range=(p2,
-                                                                       p98))
-    skimage.io.imsave('contrast_stretched'+file_type, img_contrast_stretched,
-                      plugin=None)
-    return img_contrast_stretched
-
-
-# Reverse Video
-def reverse_video(img, color_type, file_type):
-    inverted = np.zeros_like(img)
-    if color_type == 'greyscale':
-        inverted = util.invert(img)
-    elif color_type == 'color':
-        rows, columns, channels = img.shape
-        for row in range(0, rows):
-            for column in range(0, columns):
-                inverted[row, column] = [255, 255, 255] - img[row, column]
-    skimage.io.imsave('reverse_video' + file_type, inverted, plugin=None)
-    return inverted
-
-
-info, array, name = gather_data('greyscale_image_test', '.PNG')
-reverse_video(array, 'greyscale', '.PNG')
+test = Image(file_name='color_image_test', file_ext='.JPEG',
+             color_type='color')
+test.gather_data()
+test.reverse_video()
