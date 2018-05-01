59666a594c7e535900c6679b6fc3a68254650c7d
jwwangrich <jwwangrich@gmail.com>
2018-05-01 16:58:21 -0400

    fixing

--- server/Image_processing.py
+++ server/Image_processing.py
@@ -1,19 +1,23 @@
-import os
 import skimage
-import numpy as np
 import matplotlib
-matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 import base64
-import imghdr
-import math
+import timeit
+import logging
 
 from skimage import io
-from skimage import data, img_as_float
 from skimage import exposure
 from skimage import util
+matplotlib.use('TkAgg')
+
+# Set up logger
+log_format = '%(levelname)s %(asctime)s %(message)s'
+logging.basicConfig(filename='divlog.txt', format=log_format,
+                    datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG,
+                    filemode='w')
+logger = logging.getLogger()
 
 
 class Image:
@@ -38,33 +42,56 @@ class Image:
 
     # Load and Gather Image Data (Size, color / greyscale, file type)
     def gather_data(self):
+        """Gathers useful information about an input image and stores the data
+        as object attributes. Functions by taking in a base64 string,
+        decoding it then saving it as a numpy array
+        :returns self.color_type: a string stating 'greyscale' or 'color'
+        :returns self.dimensions: an array in the form of (rows, columns)
+        :returns self.image_array: a numpy array containing the digital
+        image data
+        """
         self.decode_string()
-        self.image_array = io.imread('working_image.JPEG')
+        self.image_array = io.imread('working_image' + self.file_ext)
         rows, columns, channels = self.image_array.shape
         self.dimensions = (rows, columns)
+        logger.info('Image Dimensions: %s, %s' % (rows, columns))
         if channels == 1:
             self.color_type = 'greyscale'
         else:
             self.color_type = 'color'
+        logger.info('Image Color: %s' % self.color_type)
         return self.color_type, self.dimensions, self.image_array
 
     # Decode Base64 string into workable image
     def decode_string(self):
-        self.get_file_ext()
-        fh = open("working_image"+self.file_ext, 'wb')
-        fh.write(self.image_as_string.decode('base64'))
+        """ Takes in base64 string containing image data, determines the
+        file type (either PNG or JPEG) based off this info, then saves the
+        image to disc under the name 'working_image' with the same file
+        extension as the input file
+        """
+        ext = self.get_file_ext()
+        fh = open('working_image' + ext, "wb")
+        fh.write(base64.b64decode(self.image_as_string))
+        logger.info('Base64 image successfully decoded')
 
     # Read the image file type from Base64 string
     def get_file_ext(self):
+        """Reads base64 image data and determines image file type (
+        PNG/JPEG)
+        :returns self.file_ext: string reading either '.JPEG' or '.PNG'
+        """
         string = self.image_as_string
         if string[0] == '/':
             self.file_ext = '.JPEG'
         elif string[0] == 'i':
             self.file_ext = '.PNG'
+        logger.info('Image file extension: %s' % self.file_ext)
         return self.file_ext
 
     # Generate Histogram
     def show_histogram(self):
+        """Generates histogram of image in Python
+        """
         if self.color_type == 'color':
             color = ('b', 'g', 'r')
             for i, col in enumerate(color):
@@ -84,6 +111,18 @@ class Image:
 
     # Generate plottable histogram data
     def output_histogram_data(self, hist_type):
+        """ Outputs image histogram data as arrays
+        :param hist_type: the type of data for which the histogram is being
+        produced ('orignal' - unaltered image, 'hist_eq' - image altered by
+        histogram equalization, 'rev_vid' - image altered by reverse video,
+        'contrast_stretch' - image altered by contrast stretching,
+        'log_comp' - image altered by logarithmic compression
+        :returns red_hist: the red frequency values of the image's histogram
+        :returns blue_hist: the blue frequency values of the image's histogram
+        :returns green_hist: the green frequency values of the image's
+        histogram
+        :returns x_vals: the intensity values of the image (0-255)
+        """
         red = np.zeros(self.dimensions, dtype=int)
         green = np.zeros(self.dimensions, dtype=int)
         blue = np.zeros(self.dimensions, dtype=int)
@@ -112,46 +151,81 @@ class Image:
         blue_hist = blue_data[0]
         green_hist = green_data[0]
         x_vals = range(0, 256)
-        return [[red_hist], [bluehist], [green_hist], [x_vals]]
+        logger.info('Histogram data generated')
+        return red_hist, blue_hist, green_hist, x_vals
 
     # Equalization
     def hist_eq(self):
+        """
+        Carries out histogram equalization on input image
+        :return self.hist_eq_array: numpy array containing image data of
+        altered image
+        :return run_time: the time it took to run this method in seconds
+        """
+        start_time = timeit.default_timer()
         self.hist_eq_array = exposure.equalize_hist(self.image_array)
         skimage.io.imsave('hist_equalized'+self.file_ext, self.hist_eq_array,
                           plugin=None)
-        return self.hist_eq_array
+        run_time = timeit.default_timer() - start_time
+        logger.info('Histogram equalization completed in %s seconds' %
+                    run_time)
+        return self.hist_eq_array, run_time
 
     # Contrast Stretching
     def contrast_stretch(self):
+        """
+        Carries out contrast stretching on input image
+        :return self.contrast_stretch_array: numpy array containing image
+        data of altered image
+        :return run_time: the time it took to run this method in seconds
+        """
+        start_time = timeit.default_timer()
         p2 = np.percentile(self.image_array, 2)
         p98 = np.percentile(self.image_array, 98)
         self.contrast_stretch_array = exposure.rescale_intensity(
             self.image_array, in_range=(p2, p98))
         skimage.io.imsave('contrast_stretched'+self.file_ext,
                           self.contrast_stretch_array, plugin=None)
-        return self.contrast_stretch_array
+        run_time = timeit.default_timer() - start_time
+        logger.info('Contrast stretching completed in %s seconds' %
+                    run_time)
+        return self.contrast_stretch_array, run_time
 
     # Logarithmic Compression
     def log_compression(self):
+        """
+        Carries out logarithmic compression on input image
+        :return self.log_comp_array: numpy array containing image
+        data of altered image
+        :return run_time: the time it took to run this method in seconds
+        """
+        start_time = timeit.default_timer()
         log_comp = np.zeros_like(self.image_array)
         scaling_const = 255/(np.log(255)+1)  # assumes max val is 255
-        print(scaling_const)
         rows, columns, channels = self.image_array.shape
 
         for row in range(0, rows):
             for column in range(0, columns):
                 log_comp[row, column] = scaling_const*np.log((
-                                                             np.absolute(self.
-                                                            image_array[
-                                                            row, column])))
+                                np.absolute(self.image_array[row, column])))
 
         self.log_comp_array = log_comp
         skimage.io.imsave('log_compressed'+self.file_ext,
                           self.log_comp_array, plugin=None)
-        return self.log_comp_array
+        run_time = timeit.default_timer() - start_time
+        logger.info('Logarithmic compression completed in %s seconds' %
+                    run_time)
+        return self.log_comp_array, run_time
 
     # Reverse Video
     def reverse_video(self):
+        """
+        Carries out reverse video on input image
+        :return self.rev_video_array: numpy array containing image
+        data of altered image
+        :return run_time: the time it took to run this method in seconds
+        """
+        start_time = timeit.default_timer()
         inverted = np.zeros_like(self.image_array)
         if self.color_type == 'greyscale':
             inverted = util.invert(self.image_array)
@@ -159,23 +233,45 @@ class Image:
             rows, columns, channels = self.image_array.shape
             for row in range(0, rows):
                 for column in range(0, columns):
-                    inverted[row, column] = [255, 255, 255] - \
-                                            self.image_array[row, column]
+                    image_data = self.image_array[row, column]
+                    inverted[row, column] = [255, 255, 255] - image_data
         skimage.io.imsave('reverse_video' + self.file_ext, inverted,
                           plugin=None)
         self.rev_video_array = inverted
-        return self.rev_video_array
+        run_time = timeit.default_timer() - start_time
+        logger.info('Reverse video completed in %s seconds' %
+                    run_time)
+        return self.rev_video_array, run_time
 
 
 # Encode created images into Base64
 def encode_string(filename, file_ext):
+    """Encodes image saved on disc into a base64 string
+    :param filename: filename  of image on disc which is to be encoded
+    :param file_ext: file extension of filename (.PNG/.JPEG)
+    :return string: base64 encoded image string
+    """
     with open(filename + file_ext, 'rb') as imageFile:
         string = base64.b64encode(imageFile.read())
+    logger.info('Image successfully encoded as string')
     return string
 
 
 # Generate plottable histogram data
 def output_altered_histogram_data(hist_type, file_ext):
+    """Outputs arrays containing histogram data of altered images
+    :param hist_type: the type of data for which the histogram is being
+        produced ('orignal' - unaltered image, 'hist_eq' - image altered by
+        histogram equalization, 'rev_vid' - image altered by reverse video,
+        'contrast_stretch' - image altered by contrast stretching,
+        'log_comp' - image altered by logarithmic compression
+    :param file_ext: file extension of filename (.PNG/.JPEG)
+    :returns red_hist: the red frequency values of the image's histogram
+    :returns blue_hist: the blue frequency values of the image's histogram
+    :returns green_hist: the green frequency values of the image's
+     histogram
+    :returns x_vals: the intensity values of the image (0-255)
+    """
     filename = ''
     if hist_type == 'hist_eq':
         filename = 'hist_equalized'+file_ext
@@ -183,7 +279,7 @@ def output_altered_histogram_data(hist_type, file_ext):
         filename = 'reverse_video'+file_ext
     elif hist_type == 'contrast_stretch':
         filename = 'contrast_stretched'+file_ext
-    elif hist_type == 'log_comp'+file_ext:
+    elif hist_type == 'log':
         filename = 'log_compressed'+file_ext
 
     image = io.imread(filename)
@@ -206,12 +302,19 @@ def output_altered_histogram_data(hist_type, file_ext):
     blue_hist = blue_data[0]
     green_hist = green_data[0]
     x_vals = range(0, 256)
+    logger.info('Histogram data successfully generated')
     return red_hist, blue_hist, green_hist, x_vals
 
 
 # Create callable functions for each process
 def initialize_image(image_string):
+    """ Creates instance of Image class called 'image' with the input
+    base64 string as the image_as_string attribute
+    :param image_string: base64 string containing image data
+    :return image: instance of Image class
+    """
     image = Image(image_as_string=image_string)
+    logger.info('Image initialized as instance of Image class')
     image.decode_string()
     image.gather_data()
     return image
@@ -219,48 +322,100 @@ def initialize_image(image_string):
 
 # Histogram equalization - Callable Function
 def histogram_eq_complete(image_string):
+    """ Takes in base64 string, initializes an instance of Image class
+    using this data, carries out histogram equalization, and generates
+    plottable histogram data
+    :param image_string: base64 string containing image data
+    :return image: instance of Image class
+    """
     image = initialize_image(image_string)
-    image.hist_eq()
+    hist_eq_array, run_time = image.hist_eq()
     red_hist, blue_hist, green_hist, x_vals = output_altered_histogram_data(
         'hist_eq', image.file_ext)
     base64_string = encode_string('hist_equalized', image.file_ext)
-    return [red_hist, blue_hist, green_hist, x_vals, base64_string]
+    return [red_hist, blue_hist, green_hist, x_vals, base64_string, run_time]
 
 
 # Contrast stretching - Callable Function
 def contrast_stretching_complete(image_string):
+    """ Takes in base64 string, initializes an instance of Image class
+    using this data, carries out contrast stretching, and generates
+    plottable histogram data
+    :param image_string: base64 string containing image data
+    :returns red_hist: the red frequency values of the image's histogram
+    :returns blue_hist: the blue frequency values of the image's histogram
+    :returns green_hist: the green frequency values of the image's
+     histogram
+    :returns x_vals: the intensity values of the image (0-255) for histogram
+    :returns base64_string: string containing data for altered image
+    :returns run_time: the time it took to run the contrast stretching
+    algorithm in seconds
+    """
     image = initialize_image(image_string)
-    image.contrast_stretch()
+    contrast_stretch_array, run_time = image.contrast_stretch()
     red_hist, blue_hist, green_hist, x_vals = \
         output_altered_histogram_data('contrast_stretch', image.file_ext)
     base64_string = encode_string('contrast_stretched', image.file_ext)
-    return [red_hist, blue_hist, green_hist, x_vals, base64_string]
+    return [red_hist, blue_hist, green_hist, x_vals, base64_string, run_time]
 
 
 # Reverse video - Callable Function
 def reverse_video_complete(image_string):
+    """ Takes in base64 string, initializes an instance of Image class
+    using this data, carries out reverse video, and generates
+    plottable histogram data
+    :param image_string: base64 string containing image data
+    :returns red_hist: the red frequency values of the image's histogram
+    :returns blue_hist: the blue frequency values of the image's histogram
+    :returns green_hist: the green frequency values of the image's
+     histogram
+    :returns x_vals: the intensity values of the image (0-255) for histogram
+    :returns base64_string: string containing data for altered image
+    :returns run_time: the time it took to run the reverse video
+    algorithm in seconds
+    """
     image = initialize_image(image_string)
-    image.reverse_video()
+    reverse_video_array, run_time = image.reverse_video()
     red_hist, blue_hist, green_hist, x_vals = \
         output_altered_histogram_data('rev_vid', image.file_ext)
     base64_string = encode_string('reverse_video', image.file_ext)
-    plt.plot(x_vals, red_hist)
-    plt.show()
-    return [red_hist, blue_hist, green_hist, x_vals, base64_string]
+    return [red_hist, blue_hist, green_hist, x_vals, base64_string, run_time]
 
 
 # Log Compression - Callable Function
 def log_compression_complete(image_string):
+    """ Takes in base64 string, initializes an instance of Image class
+    using this data, carries out logarithmic compression, and generates
+    plottable histogram data
+    :param image_string: base64 string containing image data
+    :returns red_hist: the red frequency values of the image's histogram
+    :returns blue_hist: the blue frequency values of the image's histogram
+    :returns green_hist: the green frequency values of the image's
+     histogram
+    :returns x_vals: the intensity values of the image (0-255) for histogram
+    :returns base64_string: string containing data for altered image
+    :returns run_time: the time it took to run the log compression algorithm
+    algorithm in seconds
+    """
     image = initialize_image(image_string)
-    image.log_compression()
+    log_comp_array, run_time = image.log_compression()
     red_hist, blue_hist, green_hist, x_vals = \
-        output_altered_histogram_data('log_comp', image.file_ext)
+        output_altered_histogram_data('log', image.file_ext)
     base64_string = encode_string('log_compressed', image.file_ext)
-    return [red_hist, blue_hist, green_hist, x_vals, base64_string]
+    return [red_hist, blue_hist, green_hist, x_vals, base64_string, run_time]
 
 
 # Output histogram data for unaltered image
 def histogram_data(image_string):
+    """ Takes in base64 string, initializes an instance of Image class
+    using this data, and generates plottable histogram data
+    :param image_string: base64 string containing image data
+    :returns red_hist: the red frequency values of the image's histogram
+    :returns blue_hist: the blue frequency values of the image's histogram
+    :returns green_hist: the green frequency values of the image's
+     histogram
+    :returns x_vals: the intensity values of the image (0-255) for histogram
+    """
     image = initialize_image(image_string)
     red_hist, blue_hist, green_hist, x_vals = image.output_histogram_data(
         'original')
