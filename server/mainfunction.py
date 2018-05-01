4024ea58ddd5ff978b29c37cf21c41028da999ad
jwwangrich <jwwangrich@gmail.com>
2018-05-01 18:53:59 -0400

    fix

--- server/mainfunction.py
+++ server/mainfunction.py
@@ -2,11 +2,13 @@ from pymodm import connect
 import models
 import datetime
 
+
 def processed_his(email, images, timing):
     user = models.User.objects.raw({"_id": email}).first()
     user.user_images_his.append(images)
     user.user_images_pro_time.append(timing)
     user.save()
+
 
 def processed_contrast(email, images, timing):
     user = models.User.objects.raw({"_id": email}).first()
@@ -14,17 +16,20 @@ def processed_contrast(email, images, timing):
     user.user_images_pro_time.append(timing)
     user.save()
 
+
 def processed_log(email, images, timing):
     user = models.User.objects.raw({"_id": email}).first()
     user.user_images_log.append(images)
     user.user_images_pro_time.append(timing)
     user.save()
 
+
 def processed_reverse(email, images, timing):
     user = models.User.objects.raw({"_id": email}).first()
     user.user_images_reverse.append(images)
     user.user_images_pro_time.append(timing)
     user.save()
+
 
 def add_images(email, image_info):
     """
@@ -50,6 +55,7 @@ def add_pro_images(email, image_info):
     user.pro_images.append(image_info)  # append image to the user's list of images
     user.save()  # save the user to the database
 
+
 def create_user(email, name):
     """
     Creates a user with the specified email and name. If the user already exists in the DB this will
@@ -65,8 +71,10 @@ def create_user(email, name):
     #u.name = name
     u.save() # save the user to the database
 
+
 def check_user(email):
     return models.User.objects.raw({"_id": email}).count()>0
+
 
 def print_user(email):
     """
@@ -77,6 +85,7 @@ def print_user(email):
     user = models.User.objects.raw({"_id": email}).first()  # Get the first user where _id=email
     print(user.email, user.name, user.images, user.pro_images)
     return user
+
 
 if __name__ == "__main__":
     connect("mongodb://vcm-3608.vm.duke.edu:27017/fp_images")  # open up connection to db
--- server/postimage.py
+++ server/postimage.py
@@ -13,22 +13,29 @@ import base64
 import PIL
 from PIL import Image
 
+
 def rm_strheader(images):
     print(type(images))
     index = images.find(b',')
     image_str = images[index + 1:]
     return image_str
 
+
 def encode_string(filename, file_ext):
     with open(filename + file_ext, 'rb') as imageFile:
         string = base64.b64encode(imageFile.read())
     return string
 
+
 with open('coach.jpeg', 'rb') as imageFile:
     images = base64.b64encode(imageFile.read())
+
+
 def get_hist_data(string):
     red_hist, blue_hist, green_hist, x_vals = Image_processing.histogram_data(string)
     return [red_hist, blue_hist, green_hist, x_vals]
+
+
 
 def pre_processing():
     with open('coach.jpeg', 'rb') as imageFile:
@@ -50,6 +57,7 @@ def pre_processing():
     print(images)
     print(images_arr)
     return images_arr
+
 
 file_jpeg = open('image_test_png.txt')
 jpeg_string = file_jpeg.read()
