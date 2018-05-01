8a5bd8955af5d29ab642c589ea009e55d31f2e01
an-schneider <35575344+an-schneider@users.noreply.github.com>
2018-05-01 14:52:19 -0400

    hardcode file extensions in test file

--- image-processing/test_Image_processing.py
+++ image-processing/test_Image_processing.py
@@ -1,9 +1,9 @@
 from Image_processing import Image
+import Image_processing
 from skimage import io
 import pytest
 import numpy as np
 import base64
-import Image_processing
 
 file_jpeg = open('image_test_jpeg.txt')
 jpeg_string = file_jpeg.read()
@@ -14,7 +14,7 @@ png_string = file_png.read()
 @pytest.mark.filterwarnings('ignore::RuntimeWarning')
 def test_reverse_video():
     # Test JPEG File
-    test = Image(image_as_string=jpeg_string)
+    test = Image(image_as_string=jpeg_string, file_ext='.jpeg')
     test.gather_data()
     output, time_to_run = test.reverse_video()
     inverted_1 = [255, 255, 255] - test.image_array[50, 50]
@@ -22,7 +22,7 @@ def test_reverse_video():
     assert output[50, 50].all() == inverted_1.all()
     assert output[0, 0].all() == inverted_2.all()
     # Test PNG file
-    test = Image(image_as_string=png_string)
+    test = Image(image_as_string=png_string, file_ext='.png')
     test.gather_data()
     output, time_to_run = test.reverse_video()
     inverted_1 = [255, 255, 255] - test.image_array[50, 50]
@@ -33,12 +33,12 @@ def test_reverse_video():
 
 def test_gather_data():
     # Test JPEG File
-    test = Image(image_as_string=jpeg_string)
+    test = Image(image_as_string=jpeg_string, file_ext='.jpeg')
     test.gather_data()
     assert test.dimensions == (777, 1200)
     assert test.color_type == 'color'
     # Test PNG File
-    test = Image(image_as_string=png_string)
+    test = Image(image_as_string=png_string, file_ext='.png')
     test.gather_data()
     assert test.dimensions == (777, 1200)
     assert test.color_type == 'color'
@@ -46,14 +46,14 @@ def test_gather_data():
 
 def test_log_compression():
     # Test JPEG File
-    test = Image(image_as_string=jpeg_string)
+    test = Image(image_as_string=jpeg_string, file_ext='.jpeg')
     test.gather_data()
     output, time_to_run = test.log_compression()
     scaling_const = 255 / (np.log(255) + 1)
     array_values = scaling_const*np.log(np.absolute(test.image_array[50, 50]))
     assert output[50, 50].all() == array_values.all()
     # Test PNG File
-    test = Image(image_as_string=png_string)
+    test = Image(image_as_string=png_string, file_ext='.png')
     test.gather_data()
     output, time_to_run = test.log_compression()
     scaling_const = 255 / (np.log(255) + 1)
@@ -63,11 +63,11 @@ def test_log_compression():
 
 def test_get_file_extension():
     # Test JPEG File
-    test = Image(image_as_string=jpeg_string)
+    test = Image(image_as_string=jpeg_string, file_ext='.jpeg')
     ext = test.get_file_ext()
     assert ext == '.JPEG'
     # Test PNG File
-    test = Image(image_as_string=png_string)
+    test = Image(image_as_string=png_string, file_ext='.png')
     ext = test.get_file_ext()
     assert ext == '.PNG'
 
@@ -76,4 +76,5 @@ def test_get_file_extension():
 test_reverse_video()
 test_gather_data()
 test_log_compression()
+test_get_file_extension()
 test_get_file_extension()
