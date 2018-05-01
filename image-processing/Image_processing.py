b1491d3d18aff651d16cfb9bdce37f6930ffda34
an-schneider <35575344+an-schneider@users.noreply.github.com>
2018-05-01 16:05:30 -0400

    fix decode string

--- image-processing/Image_processing.py
+++ image-processing/Image_processing.py
@@ -73,7 +73,7 @@ class Image:
         """
         ext = self.get_file_ext()
         fh = open('working_image' + ext, "wb")
-        fh.write(self.image_as_string.b64decode)
+        fh.write(base64.b64decode(self.image_as_string))
         logger.info('Base64 image successfully decoded')
 
     # Read the image file type from Base64 string
