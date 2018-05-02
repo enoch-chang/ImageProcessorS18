3a75b984e8a6c72864e2d6b498c04137b8ac7463
jwwangrich <jwwangrich@gmail.com>
2018-05-02 18:19:38 -0400

    add docstring

--- server/Server.py
+++ server/Server.py
@@ -20,7 +20,12 @@ CORS(app)
 
 @app.route("/api/images/<user_email>", methods=["GET"])
 def app_get_user(user_email):
-
+    """
+    app_get_user Function takes the user_email input from the user, which
+    help user to get his own images data from database with all the images
+    information.
+    :param email: str of the user email
+    """
     try:
         user_data = mainfunction.print_user(user_email)
 
@@ -39,15 +44,19 @@ def app_get_user(user_email):
 
 
 def transfer_decode(image_str):
+    """
+    Remove the header of the images base64 bytes from front-end
+    :param image_str: the input should be the base64 bytes of the image
+
+    :returns: base64 string without data header
+
+    :rtype: string
+    """
     index = image_str.find(b',')
     image_str = image_str[index + 1:]
     image_type = str(image_str, 'utf-8')
+
     return image_type
-
-
-def decode_image(image_bytes, image_id):
-    with open(image_id, 'wb') as images:
-        images.write(base64.b64decode(image_bytes))
 
 
 def get_user(user_email):
@@ -69,54 +78,43 @@ def get_user(user_email):
     return jsonify(result), 200
 
 
-def pre_processing(noheader_images, filename, images):
-    """
-    Do the pre-processing method for images that are about to be uploaded.
-    After processing, this would return a image array including the corres
-    -ponding information of the image, including: base64 str of the images,
-    filename, id, filetype, time stamp, image size and unaltered
-    histograms.
-    :param images: base64 str of the image
-    :param filename: str of the name of the image
-    """
-    images_names = filename
-    image_function = Image_processing.Image(image_as_string=noheader_images)
-    filetype = image_function.get_file_ext()
-    time_stamp = datetime.datetime.now()
-    image_data = image_function.gather_data()
-    image_size = image_data[1]
-    #histograms = Image_processing.histogram_data(noheader_images)
-    images_arr = [images, images_names, images_names, filetype,
-                  time_stamp, image_size, [[0,0],[0,0],[0,0],[0,0]]]
-
-    return images_arr
-
-
 def aft_processing(filename, protype, proc_cmd):
     """
     After the specific image processing, this function process all the
     information regarding this image. After processing, this would return a
     image array including the corresponding information of the image,
-    including: base64 str of the images, filename, id, rocess type, time
+    including: base64 str of the images, filename, id, process type, time
     stamp, time duration and processed histograms.
-    :param images: base64 str of the image
     :param filename: str of the name of the image
     :param protype: list of the post-processed image histogram
+    :param proc_cmd: the str of process command which would be Reverse Video,
+    Contrast Stretching, Log Compression and Histogram Equalization.
+
+    :returns: a python array contains all the information fo the image after
+    processing, including base64 str of the processed images, filename, id,
+    process type, time stamp, time duration and processed histograms.
+
+    :rtype: an array with string value
     """
     images = protype[4]
+
     image_type = str(images, 'utf-8')
     images_names = filename
     index = images_names.find('.')
     aft_name = images_names[:index] + ' ' + proc_cmd + images_names[index:]
+
     no_headerim = transfer_decode(images)
     image_function = Image_processing.Image(image_as_string=no_headerim)
     filetype = image_function.get_file_ext()
+
     time_stamp = datetime.datetime.now()
     time_duration = protype[5]
+
     red_his_str = str(protype[0])
     blue_his_str = str(protype[1])
     green_his_str = str(protype[2])
     x_vals_str = str(protype[3])
+
     pro_images_arr = [image_type, aft_name, images_names, filetype,
                       time_stamp, time_duration,
                       [red_his_str, blue_his_str, green_his_str, x_vals_str]]
@@ -126,7 +124,13 @@ def aft_processing(filename, protype, proc_cmd):
 
 @app.route("/api/images/create", methods=["POST"])
 def create_user():
-
+    """
+    create_user Function takes the user_email input from the user including
+    their setting email and their names. This function store and establish
+    the user profile in the database.
+    :param email: str of the user email
+    :param name: str of the user name
+    """
     r = request.get_json()
 
     email = r["email"]
@@ -139,6 +143,18 @@ def create_user():
 
 
 def rm_strheader(images):
+    """
+    Remove the header of the images base64 string from front-end
+    :param images: the input should be the base64 string of the image
+    :raises TypeError: if input is not a string
+
+    :returns: base64 string without data header
+
+    :rtype: string
+    """
+    if type(images) is not str:
+        raise TypeError('Error: imput should be a string')
+
     index = images.find(',')
     image_str = images[index + 1:]
 
@@ -147,52 +163,75 @@ def rm_strheader(images):
 
 @app.route("/api/images/upload", methods=["POST"])
 def images_post():
+    """
+    images_post Function takes the input from the user such as email, images
+    and the image filename, which performs the desired pre-processing on the
+    image and saves as an array containing image information to the database.
 
+    :param email: str of the user email
+    :param images: base64 string of the images with headers from front-end
+    :param filename: the str of images name as its specific identification
+    """
     r = request.get_json()
 
     email = r["email"]
     images = r["images"]
     filename = r["filename"]
 
-    #for im, nm in zip(images, filename):
     noheader_images = rm_strheader(images)
+
     images_names = filename
     image_function = Image_processing.Image(image_as_string=noheader_images)
     filetype = image_function.get_file_ext()
+
     time_stamp = datetime.datetime.now()
+
     image_data = image_function.gather_data()
     image_size = image_data[1]
+
     histograms = Image_processing.histogram_data(noheader_images)
     red_his_str = str(histograms[0])
     blue_his_str = str(histograms[1])
     green_his_str = str(histograms[2])
     x_vals_str = str(histograms[3])
+
     images_arr = [images, images_names, images_names, filetype,
                   time_stamp, image_size,
                   [red_his_str, blue_his_str, green_his_str, x_vals_str]]
     mainfunction.add_images(email, images_arr)
-    result = {"success": "Cong! uploading successful"}
+
+    result = {"Success": "Cong! uploading successful"}
 
     return jsonify(result), 200
 
 
 @app.route("/api/images/<email>/<filename>/process", methods=["POST"])
-def pro_images_post_his(email, filename):
+def pro_images_post(email, filename):
+    """
+    pro_images_post Function takes the input from the user such as email,
+    images the image filename as identification and process type command,
+    which performs the desired processing procedure on the image and saves
+    as an array containing image information to the database.
 
+    :param email: str of the user email
+    :param process: process types of command
+    :param images: base64 string of the images with headers from front-end
+    :param filename: the str of images name as its specific identification
+    """
     r = request.get_json()
 
     images_str = r["images"]
     image_pro_type = r["process"]
+
     filename_str = filename
     wk_images = rm_strheader(images_str)
+
     if image_pro_type == "Reverse Video":
         protype = Image_processing.reverse_video_complete(wk_images)
         images_info = aft_processing(filename_str, protype, image_pro_type)
         mainfunction.add_pro_images(email, images_info)
         result = {
-            "hope this works": "Yes it works"
-        #    "images": images,
-        #    "pro_images": images_info[4]
+            "Success": "Cong! Reverse Video completed"
         }
         return jsonify(result), 200
 
@@ -201,9 +240,7 @@ def pro_images_post_his(email, filename):
         images_info = aft_processing(filename_str, protype, image_pro_type)
         mainfunction.add_pro_images(email, images_info)
         result = {
-            "hope this works": "Yes it works"
-            #    "images": images,
-            #    "pro_images": images_info[4]
+            "Success": "Cong! Contrast Stretching completed"
         }
         return jsonify(result), 200
 
@@ -212,9 +249,7 @@ def pro_images_post_his(email, filename):
         images_info = aft_processing(filename_str, protype, image_pro_type)
         mainfunction.add_pro_images(email, images_info)
         result = {
-            "hope this works": "Yes it works"
-            #    "images": images,
-            #    "pro_images": images_info[4]
+            "Success": "Cong! Log Compression completed"
         }
         return jsonify(result), 200
 
@@ -223,9 +258,7 @@ def pro_images_post_his(email, filename):
         images_info = aft_processing(filename_str, protype, image_pro_type)
         mainfunction.add_pro_images(email, images_info)
         result = {
-            "hope this works": "Yes it works"
-            #    "images": images,
-            #    "pro_images": images_info[4]
+            "Success": "Cong! Histogram Equalization completed"
         }
         return jsonify(result), 200
 
